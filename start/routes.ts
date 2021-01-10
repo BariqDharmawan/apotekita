b806ce67265f837a3acff58b028aebb5df954205
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-01-10 18:55:01 +0700

    [UPDATE] routing

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'HomeController.index').as('homepage')
-Route.resource('obat', 'ObatController')
-Route.resource('penjualan', 'PenjualanController').only(['index', 'store', 'edit', 'update'])
+Route.get('persediaan', 'ObatController.persediaan').as('persediaan')
+Route.resource('obat', 'ObatController').except(['show'])
+Route.resource('transaksi', 'PenjualanController').only(['index', 'store', 'edit', 'update'])
