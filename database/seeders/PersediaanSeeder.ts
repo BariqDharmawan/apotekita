bbc8adbf7ed83ba541ecc98ef312c9f2462a66cf
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-01-15 03:03:43 +0700

    [UPDATE] refactoring seeder

--- database/seeders/PersediaanSeeder.ts
+++ database/seeders/PersediaanSeeder.ts
@@ -3,7 +3,7 @@ import Persediaan from 'App/Models/Persediaan'
 
 export default class PersediaanSeederSeeder extends BaseSeeder {
     public async run() {
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 0; i < 11; i++) {
             await Persediaan.create({
                 kd_obat: `kode-obat${i}`,
                 jumlah_persediaan: Math.round(Math.random() * 100),
