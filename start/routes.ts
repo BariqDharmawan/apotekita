d5cc7c7f63c605091980bff5a99a948c7e05ccc5
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-01-24 21:03:25 +0700

    [UPDATE] search obat feature

--- start/routes.ts
+++ start/routes.ts
@@ -1,8 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'HomeController.index').as('homepage')
-Route.get('persediaan', 'ObatController.persediaan').as('persediaan')
-Route.resource('obat', 'ObatController').except(['show'])
-Route.get('obat/cari/:obat', 'ObatController.show').as('obat.show')
+Route.resource('persediaan-obat', 'PersediaanController')
+Route.resource('obat', 'ObatController').except(['show', 'update', 'edit'])
+Route.get('obat/cari', 'ObatController.show').as('obat.show')
 Route.resource('transaksi', 'PenjualanController').only(['index', 'store', 'edit', 'update'])
 Route.get('transaksi/filter/:bulan', 'PenjualanController.filterBulan').as('filter-bulan')
